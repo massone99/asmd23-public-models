0528665d9e59e1de1d80253152f9b0d731dde481
Lorenzo Massone <lorenzomassone@gmail.com>
2025-02-15 16:42:44 +0100

    doc: added better documentation of PetriNet.scala

--- src/main/scala/u06/modelling/PetriNet.scala
+++ src/main/scala/u06/modelling/PetriNet.scala
@@ -3,25 +3,62 @@ package u06.modelling
 import u06.utils.MultiSet
 
 object PetriNet:
-  // pre-conditions, effects, inhibition
+  /** A Transition in a Petri Net consists of:
+   * @param cond Pre-conditions (tokens that must be present)
+   * @param eff Effects (tokens produced by firing)
+   * @param inh Inhibitor arcs (tokens that prevent firing)
+   *    In Petri Nets, inhibitor arcs prevent a transition from firing 
+   *    when certain places contain tokens. It's a marking that prevents this transition from happening
+   */
   case class Transition[P](cond: MultiSet[P], eff: MultiSet[P], inh: MultiSet[P])
+  // The type PetriNet[P] is defined as a Set of Transitions
   type PetriNet[P] = Set[Transition[P]]
+  // The type Marking[P] is defined as a MultiSet
   type Marking[P] = MultiSet[P]
+
+  // Why P? Because a state can be modeled using any type (integer, string, etc.) or class
+  // See TryMSet.scala for an example of usage
 
   // factory of A Petri Net
   def apply[P](transitions: Transition[P]*): PetriNet[P] = transitions.toSet
 
-  // factory of a System, as a toSystem method
+  // Defining extension methods for PetriNet
   extension [P](pn: PetriNet[P])
-    def toSystem: System[Marking[P]] = m =>
+    // factory of a System, as a toSystem method
+    def toSystem: System[Marking[P]] = marking =>
       for
-        Transition(cond, eff, inh) <- pn // get any transition
-        if m disjoined inh // check inhibition
-        out <- m extract cond       // remove precondition
-      yield out union eff           // add effect
+        // <- is the generator operator (similar to in in other languages)
+        // like writing: for transition in pn
+        Transition(precondition, markingProducedByTransition, inhbitorMarking) <- pn // Get a transition with its inhibitors
+        // m disjoined inh returns true if the two multisets have no elements in common
+        // m and inh are both Marking[P] (MultiSet[P])
+        if marking disjoined inhbitorMarking
+        // like writing: for out in m
+        // m extract cond => rimuove da m tutti gli elementi presenti in cond
+        // considerando che cond è il marking necessario per far scattare la transizione
+        // ciò significa rimuovere i token necessari per far scattare la transizione
+        // (i.e. pagare il costo!)
 
-  // fancy syntax to create transition rules
+        // out rappresenta il marking originale meno i token necessari per far scattare la transizione
+        // che sono stati rimossi con m extract cond
+        markingAfterConsumingCond <- marking extract precondition                // If check passes, try to consume input tokens
+      yield markingAfterConsumingCond union markingProducedByTransition                    // If successful, produce output tokens
+
+
+  // Defining extension methods for Marking
   extension [P](self: Marking[P])
+    /** Syntactic sugar for creating basic transitions
+     * Enables writing transitions as: place1 ~~> place2
+     * @return A new Transition with no inhibitor arcs
+     */
     def ~~> (y: Marking[P]): Transition[P] = Transition(self, y, MultiSet())
+  
+  // Defining extension methods for Transition
   extension [P](self: Transition[P])
-    def ^^^ (z: Marking[P]): Transition[P] = self.copy(inh = z)
+    /** Syntactic sugar for adding inhibitor arcs
+     * Enables writing transitions as: (place1 ~~> place2) ^^^ place3
+     * @return A new Transition with the specified inhibitor arc
+     */
+    def ^^^ (z: Marking[P]): Transition[P] = 
+      // self.copy(inh = z): Creates a new transition copying all fields from self but replacing the inh field with z
+      self.copy(inh = z)
--- src/main/scala/u06/utils/MultiSet.scala
+++ src/main/scala/u06/utils/MultiSet.scala
@@ -1,27 +1,66 @@
 package u06.utils
 
-// A multiset datatype
+/** A multiset (or bag) implementation that allows elements to appear multiple times.
+ *  
+ * Used to model markings in Petri Nets, where tokens can be distributed across places
+ * with multiple tokens allowed in the same place.
+ * 
+ * Example: M = {N, N, T, C} represents a marking where:
+ * - Place N contains two tokens
+ * - Places T and C contain one token each
+ */
 trait MultiSet[A] extends (A => Int):
+  /** Combines this multiset with another, summing multiplicities */
   def union(m: MultiSet[A]): MultiSet[A]
+  
+  /** Removes elements of m from this multiset */
   def diff(m: MultiSet[A]): MultiSet[A]
+  
+  /** Checks if this multiset and m have no elements in common */
   def disjoined(m: MultiSet[A]): Boolean
+  
+  /** Returns the total number of elements counting multiplicities */
   def size: Int
+  
+  /** Checks if m is contained within this multiset */
   def matches(m: MultiSet[A]): Boolean
+  
+  /** Attempts to remove m from this multiset
+   * @return Some(result) if successful, None if not possible
+   */
   def extract(m: MultiSet[A]): Option[MultiSet[A]]
+  
+  /** @return List representation with elements repeated according to multiplicities */
   def asList: List[A]
+  
+  /** @return Map from elements to their multiplicities */
   def asMap: Map[A,Int]
+  
+  /** @return Iterator over unique elements */
   def iterator: Iterator[A]
 
-// Functional-style helpers/implementation
+/** Factory methods and implementation for MultiSet */
 object MultiSet:
-  // Factories
+  /** Creates a MultiSet from varargs 
+   * @param l The elements to include
+   */
   def apply[A](l: A*): MultiSet[A] = new MultiSetImpl(l.toList)
+  
+  /** Creates a MultiSet from a List
+   * @param l The list of elements
+   */
   def ofList[A](l: List[A]): MultiSet[A] = new MultiSetImpl(l)
+  
+  /** Creates a MultiSet from a Map of multiplicities
+   * @param m Map from elements to their counts
+   */
   def ofMap[A](m: Map[A,Int]): MultiSet[A] = MultiSetImpl(m)
 
-  // Hidden reference implementation
+  /** Private implementation using a Map to store multiplicities */
   private case class MultiSetImpl[A](asMap: Map[A,Int]) extends MultiSet[A]:
+    /** Converts a list to a multiset by counting occurrences */
     def this(list: List[A]) = this(list.groupBy(a => a).map((a,n) => (a, n.size)))
+    
     override val asList =
       asMap.toList.flatMap((a,n) => List.fill(n)(a))
 
@@ -32,6 +71,7 @@ object MultiSet:
     override def size = asList.size
     override def matches(m: MultiSet[A]) = extract(m).isDefined
     override def extract(m: MultiSet[A]) =
+      // filter keeps the result only if size decreased by exactly m.size
       Some(this diff m) filter (_.size == size - m.size)
     override def iterator = asMap.keysIterator
     override def toString = s"{${asList.mkString("|")}}"
